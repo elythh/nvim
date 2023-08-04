e5682db0ddc5b20021802d3c02d92928ad629c99
elythh <elyth@tuta.io>
2023-08-05 01:20:19 +0200

    feat(colorscheme): mountain

---
+++ lua/themes/schemes/mountain.lua
@@ -0,0 +1,30 @@
+function M.get_colors()
+  return {
+    comment = "#4c4c4c",
+    contrast = "#22292d",
+    background = "#0f0f0f",
+    darker = "#1d2125",
+    black = "#262626",
+    foreground = "#f0f0f0",
+    cursorline = "#22292d",
+    cursor = "#f0f0f0",
+    color0 = "#0f0f0f",
+    color1 = "#ac8a8c",
+    color2 = "#8aac8b",
+    color3 = "#aca98a",
+    color4 = "#8f8aac",
+    color5 = "#ac8aac",
+    color6 = "#8aacab",
+    color7 = "#f0f0f0",
+    color8 = "#262626",
+    color9 = "#c49ea0",
+    color10 = "#9ec49f",
+    color11 = "#c4c19e",
+    color12 = "#a39ec4",
+    color13 = "#c49ec4",
+    color14 = "#9ec3c4",
+    color15 = "#e7e7e7",
+  }
+end
+
+return M
