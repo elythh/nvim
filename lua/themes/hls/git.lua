865f743ee86e7dfb6b02d5dc728ae802affbe81a
elythh <elyth@tuta.io>
2023-08-11 04:24:02 +0200

    refact(gitsigns): hls line blame

--- lua/themes/hls/git.lua
+++ lua/themes/hls/git.lua
@@ -4,4 +4,5 @@ return {
   GitSignsAdd = { fg = colors.color4 },    -- diff mode: Added line |diff.txt|
   GitSignsChange = { fg = colors.color5 }, -- diff mode: Changed line |diff.txt|
   GitSignsDelete = { fg = colors.color1 }, -- diff mode: Deleted line |diff.txt|
+  GitSignsCurrentLineBlame = { fg = colors.comment },
 }
