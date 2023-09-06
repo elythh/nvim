3acc1211b747dfe5f9fe4ee2bebd856c6bb8a7d4
elythh <elyth@tuta.io>
2023-09-06 14:06:55 +0200

    maint(stl): remove cmd for old statusline

--- lua/core/cmds.lua
+++ lua/core/cmds.lua
@@ -6,22 +6,3 @@ local has_value = function(tab, val)
   end
   return false
 end
-
-local available_themes = { 'minimal', 'fancy' }
-vim.api.nvim_create_user_command(
-  'StatuslineGlowup',
-  function(opts)
-    if has_value(available_themes, opts.args) then
-      vim.g.statusStyle = opts.args
-      require("ui.stl").setup(opts.args)
-    else
-      print 'Invalid Theme'
-    end
-  end,
-  {
-    nargs = 1,
-    complete = function()
-      return available_themes
-    end,
-  }
-)
