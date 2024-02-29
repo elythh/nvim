1cbebe1959f32c1f1017e77ed64914976de1262a
elythh <gwenchlan.lekerneau@radiofrance.com>
2024-02-29 13:55:13 +0100

    :sparkles: (formatter): liquidsoap-prettier

---
+++ lua/plugs/lsp/conform.lua
@@ -0,0 +1,22 @@
+require("conform").setup({
+  format_on_save = {
+    lsp_fallback = true,
+    timeoutMs = 500,
+  },
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- Use a sub-list to run only the first available formatter
+    javascript = { { "prettierd", "prettier" } },
+    --
+    liquidsoap = { "liquidsoap-prettier" },
+  },
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
