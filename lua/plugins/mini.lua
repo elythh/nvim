d805e42dedd408f25fbeb0f053c123fba31d3f8f
elyth <gwen@omg.lol>
2025-04-30 21:54:47 +0200

    feat: mini.hipattersn

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,6 +4,27 @@ return {
 		event = "VeryLazy",
 		lazy = true,
 		version = false,
+		config = function()
+			local mini_hipatterns = require("mini.hipatterns")
+
+			mini_hipatterns.setup({
+				highlighters = {
+					-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+					-- Highlight hex color strings (`#rrggbb`) using that color
+					hex_color = mini_hipatterns.gen_highlighter.hex_color(),
+				},
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.hipatterns",
+		lazy = false,
+		version = false,
 	},
 	{
 		"echasnovski/mini.starter",
