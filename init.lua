18c6342b338ebd462659ee529f31d945d0b392ff
elyth <gwen@omg.lol>
2025-05-05 20:28:54 +0200

    feat: lsp fold au

--- init.lua
+++ init.lua
@@ -84,3 +84,14 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- disable new line autocomment
 vim.cmd([[autocmd FileType * set formatoptions-=ro]])
+
+-- Prefer LSP folding if client supports it. Allows to fold comments and more
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
