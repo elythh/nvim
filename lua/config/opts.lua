bd181b1317c235daa5e42cb44e5bfc7682883e2f
elyth <gwen@omg.lol>
2025-05-05 20:28:44 +0200

    feat: opt refact

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -1,26 +1,59 @@
 local o = vim.opt
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-o.fillchars = { eob = " " }
-o.termguicolors = true
-o.shiftwidth = 2
+-- Better UX
+o.autowriteall = true
+o.splitright = true
+o.splitbelow = true
+o.swapfile = false
+o.undofile = true
+o.clipboard = "unnamedplus"
+o.scrolloff = 20
+
+-- Indents settings
 o.tabstop = 2
+o.shiftwidth = 2
 o.expandtab = true
 o.smartindent = true
-o.laststatus = 3
-o.showmode = false
-o.smartcase = true
-o.foldmethod = "manual"
-o.mouse = "a"
+
+-- UI
+o.showcmd = false
 o.number = true
 o.relativenumber = true
-o.numberwidth = 2
-o.ruler = false
+o.showtabline = 1
 o.signcolumn = "yes"
-o.splitbelow = true
-o.splitright = true
-o.timeoutlen = 400
-o.undofile = true
-o.conceallevel = 1
-o.winborder = "single"
-o.shortmess:append("sI")
-o.whichwrap:append("<>[]hl")
+o.breakindent = true
+vim.g.termguicolors = true
+o.updatetime = 250
+o.showmode = false
+o.laststatus = 3
+o.fillchars = { eob = " " }
+vim.go.guicursor = "n-v-sm:block,i-t-ci-ve-c:ver25,r-cr-o:hor20"
+-- Search
+o.ignorecase = true
+o.smartcase = true
+
+vim.diagnostic.config({
+	virtual_text = true,
+	float = { border = "rounded" },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+	},
+})
+
+-- https://www.reddit.com/r/neovim/comments/1jmqd7t/sorry_ufo_these_7_lines_replaced_you/
+-- Nice and simple folding:
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ""
+o.foldcolumn = "0"
+o.fillchars:append({ fold = " " })
