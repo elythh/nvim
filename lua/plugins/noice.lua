93aae6657c9a0253f9de2e4bcc72cbad97a5c1de
elyth <gwen@omg.lol>
2025-05-05 19:57:46 +0200

    style: noice nice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,30 +1,46 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    notify = {
-      enabled = false,
-      view = "notify",
-    },
-    messages = {
-      enabled = true,
-      view = "mini",
-    },
-    lsp = {
-      message = {
-        enabled = false,
-      },
-      progress = {
-        enabled = false,
-        view = "mini",
-      },
-    },
-    popupmenu = {
-      enabled = true,
-      backend = "nui",
-    },
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-  }
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			lsp_doc_border = true, -- add a border to hover docs and signature help
+		},
+		notify = {
+			enabled = true,
+			view = "notify",
+		},
+		messages = {
+			enabled = true,
+			view = "mini",
+		},
+		lsp = {
+			message = {
+				enabled = false,
+			},
+			progress = {
+				enabled = true,
+				view = "mini",
+			},
+		},
+		popupmenu = {
+			enabled = true,
+			backend = "nui",
+		},
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "written",
+				},
+				opts = { skip = true },
+			},
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
 }
