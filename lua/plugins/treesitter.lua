fa9ddf27a8b82d970cdf7ca2eb146532d6828ffb
elyth <gwen@omg.lol>
2025-05-05 19:50:59 +0200

    feat: treesj and treesitter stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,79 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		lazy = true,
-		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-refactor",
-		},
+		event = { "BufReadPost", "BufNewFile" },
 		config = function()
+			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"yaml",
-					"hcl",
-					"c",
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"markdown",
-					"markdown_inline",
-				},
-				sync_install = false,
 				auto_install = true,
-				ignore_install = { "javascript" },
 				highlight = {
 					enable = true,
 				},
-				modules = {},
 			})
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		lazy = false,
+		config = function()
+			local swap = require("nvim-treesitter.textobjects.swap")
+			vim.keymap.set("n", "{", function()
+				swap.swap_previous("@parameter.inner")
+			end)
+			vim.keymap.set("n", "}", function()
+				swap.swap_next("@parameter.inner")
+			end)
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+							["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+						},
+						include_surrounding_whitespace = true,
+					},
+					move = {
+						enable = true,
+						goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+						goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+						goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+						goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+					},
+				},
+			})
+		end,
+	},
+	{
+		"Wansmer/treesj",
+		keys = {
+			{
+				"gs",
+				function()
+					require("treesj").toggle()
+				end,
+				mode = "n",
+			},
+			{
+				"gS",
+				function()
+					require("treesj").toggle({ split = { recursive = true } })
+				end,
+				mode = "n",
+			},
+		},
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("treesj").setup({
+				max_join_length = 1000000,
+				use_default_keymaps = false,
+			})
+		end,
 	},
 }
