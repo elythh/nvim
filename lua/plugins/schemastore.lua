09129b885cf61276575627c47e06575bad4848b4
elyth <gwen@omg.lol>
2025-05-17 00:22:28 +0200

    wip

--- lua/plugins/schemastore.lua
+++ lua/plugins/schemastore.lua
@@ -1,3 +1,12 @@
 return {
-	"b0o/schemastore.nvim",
+	{ "b0o/schemastore.nvim" },
+	{
+		"cwrau/yaml-schema-detect.nvim",
+		config = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"folke/which-key.nvim",
+		},
+		ft = { "yaml" },
+	},
 }
