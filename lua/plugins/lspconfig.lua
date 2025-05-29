71d7583ba7ec09ef1917677ac7db220be9add3cd
elyth <gwen@omg.lol>
2025-05-29 19:26:20 +0200

    feat: lspsaga

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -68,4 +68,82 @@ return {
 			})
 		end,
 	},
+	{
+		"nvimdev/lspsaga.nvim",
+		config = function()
+			require("lspsaga").setup({
+				beacon = {
+					enable = true,
+				},
+				ui = {
+					border = "rounded",
+					codeAction = "💡",
+				},
+				hover = {
+					openCmd = "!floorp",
+					openLink = "gx",
+				},
+				diagnostic = {
+					borderFollow = true,
+					diagnosticOnlyCurrent = false,
+					showCodeAction = true,
+				},
+				symbolInWinbar = {
+					enable = true,
+				},
+				codeAction = {
+					extendGitSigns = false,
+					showServerName = true,
+					onlyInCursor = true,
+					numShortcut = true,
+					keys = {
+						exec = "<CR>",
+						quit = {
+							"<Esc>",
+							"q",
+						},
+					},
+				},
+				lightbulb = {
+					enable = false,
+					sign = false,
+					virtualText = true,
+				},
+				implement = {
+					enable = false,
+				},
+				rename = {
+					autoSave = false,
+					keys = {
+						exec = "<CR>",
+						quit = {
+							"<C-k>",
+							"<Esc>",
+						},
+						select = "x",
+					},
+				},
+				outline = {
+					autoClose = true,
+					autoPreview = true,
+					closeAfterJump = true,
+					layout = "normal",
+					winPosition = "right",
+					keys = {
+						jump = "e",
+						quit = "q",
+						toggleOrJump = "o",
+					},
+				},
+				scrollPreview = {
+					scrollDown = "<C-f>",
+					scrollUp = "<C-b>",
+				},
+			})
+		end,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter", -- optional
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	},
 }
