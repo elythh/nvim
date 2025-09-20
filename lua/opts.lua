44521dddd460aeb13e40c1b750a978f7547c761d
elyth <gwen@omg.lol>
2025-09-21 01:38:08 +0200

    feat: autocmd

--- lua/opts.lua
+++ lua/opts.lua
@@ -23,6 +23,15 @@ opts.initial = function()
 
   opt.number = true
   opt.relativenumber = true
+  -- Not relative when insert mode
+  vim.api.nvim_create_autocmd(
+    { "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
+    { pattern = "*", command = "if &nu && mode() != 'i' | set rnu | endif" }
+  )
+  vim.api.nvim_create_autocmd(
+    { "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
+    { pattern = "*", command = "if &nu | set nornu | endif" }
+  )
 
   opt.signcolumn = "yes"
   opt.splitbelow = true
@@ -47,19 +56,6 @@ opts.initial = function()
 end
 
 opts.final = function()
-  -- mason, write correct names only
-  vim.api.nvim_create_user_command("MasonInstallAll", function()
-    vim.cmd "MasonInstall css-lsp html-lsp lua-language-server typescript-language-server stylua prettier"
-  end, {})
-
-  -- format on save
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-      require("conform").format { bufnr = args.buf }
-    end,
-  })
-
   vim.diagnostic.config {
     underline = {
       severity = { min = vim.diagnostic.severity.WARN },
@@ -119,6 +115,60 @@ opts.final = function()
   -- Create autocommand to trigger on CursorHold
   vim.api.nvim_create_autocmd("CursorHold", {
     callback = show_custom_diagnostic_float,
+  })
+
+  -- Open help in a vertical split
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "help",
+    callback = function()
+      vim.cmd "wincmd L"
+    end,
+  })
+
+  -- Close Snacks prompt in insert mode by clicking escape
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "snacks_picker_input",
+    callback = function()
+      vim.api.nvim_buf_set_keymap(0, "i", "<ESC>", "<ESC>:close!<CR>", { noremap = true, silent = true })
+    end,
+  })
+
+  -- Highlight yank text
+  vim.api.nvim_create_autocmd("TextYankPost", {
+    pattern = "*",
+    callback = function()
+      vim.highlight.on_yank { timeout = 500 }
+    end,
+  })
+
+  -- Enter git buffer in insert mode
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "gitcommit", "gitrebase" },
+    callback = function()
+      vim.cmd "startinsert | 1"
+    end,
+  })
+
+  -- q to quit on help
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "help" },
+    callback = function()
+      vim.keymap.set("n", "q", "<cmd>q<CR>", { buffer = 0 })
+    end,
+  })
+
+  -- disable new line autocomment
+  vim.cmd [[autocmd FileType * set formatoptions-=ro]]
+
+  -- Prefer LSP folding if client supports it. Allows to fold comments and more
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      if client:supports_method "textDocument/foldingRange" then
+        local win = vim.api.nvim_get_current_win()
+        vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+      end
+    end,
   })
 
   -- add binaries installed by mason.nvim to path
