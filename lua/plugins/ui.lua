bb94a2e10365f4020178faeb12691d228da4f226
Gwenc'hlan Le Kerneau <gwen@omg.lol>
2025-09-20 17:47:56 +0200

    style: lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -55,84 +55,60 @@ return {
 
   {
     "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = true,
     event = "VeryLazy",
-    init = function()
-      vim.g.lualine_laststatus = vim.o.laststatus
-      if vim.fn.argc(-1) > 0 then
-        vim.o.statusline = " "
-      else
-        vim.o.laststatus = 0
-      end
-    end,
     config = function()
-      local clients_lsp = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-
-        local clients = vim.lsp.get_clients { bufnr = bufnr }
-        if next(clients) == nil then
-          return ""
-        end
-
-        local c = {}
-        for _, client in pairs(clients) do
-          table.insert(c, client.name)
-        end
-        return " " .. table.concat(c, "|")
-      end
-
-      local M = {
+      require("lualine").setup {
         options = {
-          theme = "tokyonight",
-          component_separators = "",
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = { "NvimTree", "snacks_dashboard" },
+          global_status = true,
+          disabled_filetypes = {
+            statusline = {
+              "ministarter",
+            },
+          },
+          component_separators = {
+            left = "",
+            right = "",
+          },
+          section_separators = {
+            left = "",
+            right = "",
+          },
+          theme = {
+            normal = {
+              y = { bg = nil },
+            },
+            insert = {
+              y = { bg = nil },
+            },
+            command = {
+              y = { bg = nil },
+            },
+            visual = {
+              y = { bg = nil },
+            },
+            replace = {
+              y = { bg = nil },
+            },
+          },
         },
         sections = {
-          lualine_a = {
-            { "mode", icon = "" },
-          },
-          lualine_b = {
-            {
-              "filetype",
-              icon_only = true,
-              padding = { left = 1, right = 0 },
-            },
-            "filename",
-          },
+          lualine_a = { "mode" },
+          lualine_b = { "filename" },
           lualine_c = {
-            {
-              "branch",
-              icon = "",
-            },
-            {
-              "diff",
-              symbols = { added = " ", modified = " ", removed = " " },
-              colored = false,
-            },
+            "branch",
+            "diff",
           },
-          lualine_x = {
-            {
-              "diagnostics",
-              symbols = { error = " ", warn = " ", hint = " ", info = " " },
-              update_in_insert = true,
-            },
+          lualine_x = { "diagnostics" },
+          lualine_y = {
+            "filetype",
+            "progress",
+            "location",
           },
-          lualine_y = { clients_lsp },
-          lualine_z = {
-            { "location", icon = "" },
-          },
-        },
-        inactive_sections = {
-          lualine_a = { "filename" },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { "location" },
+          lualine_z = {},
         },
       }
-
-      require("lualine").setup(M)
     end,
   },
 
