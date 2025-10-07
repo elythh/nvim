10a54c3ee3e5e7ad580a240e9dc6c6d41ac8a634
elyth <gwen@omg.lol>
2025-10-07 16:02:43 +0200

    style: default mini.pick window

--- lua/plugins/mini_pick.lua
+++ lua/plugins/mini_pick.lua
@@ -262,44 +262,6 @@ return {
         choose        = nil,
         choose_marked = nil,
       },
-
-      -- Window related options
-      window = {
-        config = function()
-          local height, width, starts, ends
-          local win_width = vim.o.columns
-          local win_height = vim.o.lines
-
-          if win_height <= 25 then
-            height = math.min(win_height, 18)
-            width = win_width
-            starts = 1
-            ends = win_height
-          else
-            width = math.floor(win_width * 0.5) -- 50%
-            height = math.floor(win_height * 0.3) -- 30%
-            starts = math.floor((win_width - width) / 2)
-            -- center prompt: height * (50% + 30%)
-            -- center window: height * [50% + (30% / 2)]
-            ends = math.floor(win_height * 0.65)
-          end
-
-          return {
-            col = starts,
-            row = ends,
-            height = height,
-            width = width,
-            style = 'minimal',
-            -- border = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
-          }
-        end,
-
-        -- String to use as cursor in prompt
-        prompt_caret = '|',
-
-        -- String to use as prefix in prompt
-        prompt_prefix = '',
-      },
     }
   end,
 }
