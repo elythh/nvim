09bfce62b1aa16784c154a6e7ad1c77b1a32f12f
elyth <gwen@omg.lol>
2026-04-21 15:07:34 +0200

    fix: bufferline colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,9 +5,36 @@ return {
       require("tokyonight").setup {
         transparent = true,
         on_highlights = function(hl, c)
+          local function set_transparent(group)
+            local existing = hl[group]
+            if type(existing) == "table" then
+              hl[group] = vim.tbl_extend("force", existing, { bg = c.none })
+            else
+              hl[group] = { bg = c.none }
+            end
+          end
+
           hl.TabLineFill = {
             bg = c.none,
           }
+          set_transparent "TabLineSel"
+          set_transparent "BufferLineFill"
+          set_transparent "BufferLineBackground"
+          set_transparent "BufferLineBuffer"
+          set_transparent "BufferLineBufferVisible"
+          set_transparent "BufferLineBufferSelected"
+          set_transparent "BufferLineCloseButton"
+          set_transparent "BufferLineCloseButtonVisible"
+          set_transparent "BufferLineCloseButtonSelected"
+          set_transparent "BufferLineModified"
+          set_transparent "BufferLineModifiedVisible"
+          set_transparent "BufferLineModifiedSelected"
+          set_transparent "BufferLineSeparator"
+          set_transparent "BufferLineSeparatorVisible"
+          set_transparent "BufferLineSeparatorSelected"
+          set_transparent "BufferLineDiagnostic"
+          set_transparent "BufferLineDiagnosticVisible"
+          set_transparent "BufferLineDiagnosticSelected"
           hl.BlinkCmpMenu = {
             bg = c.bg_dark,
           }
